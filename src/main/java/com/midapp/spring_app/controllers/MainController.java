d4b9fe9baefc099af729f797f43d74a6338d5d4c
MIA1kl <mia01kln@gmail.com>
2021-03-15 01:15:34 +0600

    Home page and home-details page were updated

--- src/main/java/com/midapp/spring_app/controllers/MainController.java
+++ src/main/java/com/midapp/spring_app/controllers/MainController.java
@@ -1,17 +1,39 @@
 package com.midapp.spring_app.controllers;
 
+import com.midapp.spring_app.models.Post;
+import com.midapp.spring_app.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Controller
 public class MainController {
+    @Autowired
+    private PostRepository postRepository;
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title","Main Page");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "home";
+    }
+
+    @GetMapping("/home-details/{id}")
+    public String homeMore(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/";
+        }
+        Optional<Post> post=postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "home-details";
     }
 
     @GetMapping("/about")
